6eb7a715cfafb45c692b3ebf2c2870c85349fe23
Dennis Corsi <denniscorsi@gmail.com>
2024-02-29 12:35:16 -0500

    Add props for imgSrc, name, and location to DoctorCard

--- client/DoctorCard.tsx
+++ client/DoctorCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/system';
+import { Typography } from '@mui/material';
 
 const DoctorPaper = styled(Paper)({
   // width: '100px',
@@ -10,19 +11,21 @@ const DoctorPaper = styled(Paper)({
 
 interface DoctorCardProps {
   imgSrc: string;
+  name: string;
+  location: string;
 }
 
-const DoctorCard: React.FC<DoctorCardProps> = ({ imgSrc }) => {
+const DoctorCard: React.FC<DoctorCardProps> = ({ imgSrc, name, location }) => {
   return (
     <Box>
       <DoctorPaper elevation={12} square={false}>
         <Box sx={{ height: '200px', overflow: 'hidden' }}>
           <img className="doctor" src={imgSrc}></img>
         </Box>
-        <Box>
-          <p>info</p>
-          <p>info</p>
-          <p>info</p>
+        <Box p={1}>
+          <Typography variant="h5">{name}</Typography>
+          <Typography variant="body1">{location}</Typography>
+          <Typography variant="body1">{name}</Typography>
         </Box>
       </DoctorPaper>
     </Box>
--- client/app.tsx
+++ client/app.tsx
@@ -13,6 +13,8 @@ const App: React.FC = () => {
             imgSrc={
               'https://www.shutterstock.com/image-photo/medical-concept-indian-beautiful-female-600nw-1635029716.jpg'
             }
+            name="Doctor Maggie"
+            location="New York, NY"
           />
         </Grid>
         <Grid item xs={4}>
@@ -20,6 +22,8 @@ const App: React.FC = () => {
             imgSrc={
               'https://wp.globaluniversitysystems.com/mua/wp-content/uploads/sites/10/2023/03/board-certified-doctor-meaning.webp'
             }
+            name="Doctor Maggie"
+            location="New York, NY"
           />
         </Grid>
         <Grid item xs={4}>
@@ -27,6 +31,8 @@ const App: React.FC = () => {
             imgSrc={
               'https://www.hackensackmeridianhealth.org/-/media/project/hmh/hmh/public/healthu-images/patient-perspectives/20220826-ofer-avi-werthaim-md.png'
             }
+            name="Doctor Maggie"
+            location="New York, NY"
           />
         </Grid>
         <Grid item xs={4}>
@@ -34,6 +40,8 @@ const App: React.FC = () => {
             imgSrc={
               'https://images.ctfassets.net/pxcfulgsd9e2/3SeHNiR1vj8XTHzdTHHDnU/95d6df23dbdc6c587581ac3bd49e787c/Primary-care-duplex-hero-3840x2160.png?f=top&fit=fill&fm=png&h=786&q=85&w=1396'
             }
+            name="Doctor Maggie"
+            location="New York, NY"
           />
         </Grid>
       </Grid>
